8becbb7c9e6540c8082b683909cb0a97904ec510
ViceElez <vice.elezovic12@gmail.com>
2025-03-06 01:41:11 +0100

    add filter design and pagination

--- nasaPage/src/pages/MarsRoverPage.tsx
+++ nasaPage/src/pages/MarsRoverPage.tsx
@@ -35,21 +35,42 @@ export const MarsRoverPage = () => {
 
 
     return(
-        <div className="mars-page-content">
-            <div className='mars-rover-list'>
-                {photos.map((data)=>(
-                    <MarsRover
-                        key={data.id}
-                        image={data.img_src}
-                    />
-                ))}
+        <div className='mars-page'>
+            <nav>
+                <span>Sort</span>
+                <select id="select-filter">
+                    <option value="Camera">Camera</option>
+                    <option value="Rover">Rover</option>
+                </select>
+                <select  id="rover-dropdown">
+                   <option value="Curiosity">Curiosity</option>
+                   <option value="Opportunity">Opportunity</option>
+                   <option value="Spirit">Spirit</option>
+                </select>
+                <select  id="camera-dropdown">
+
+                </select>
+            </nav>
+            <div className="mars-page-content">
+                <div className='mars-rover-list'>
+                    {photos.map((data)=>(
+                        <MarsRover
+                            key={data.id}
+                            image={data.img_src}
+                        />
+                    ))}
+                </div>
             </div>
+            <footer>
+                    <button onClick={() => setPage(page - 1)} disabled={page === 1}>Previous</button>
+                    <button onClick={() => setPage(page + 1)}>Next</button>
+                </footer>
             <div className="circle-container">
-                <div className='circle circle-1'></div>
-                <div className='circle circle-2'></div>
-                <div className='circle circle-3'></div>
+                 <div className='circle circle-1'></div>
+                 <div className='circle circle-2'></div>
+                 <div className='circle circle-3'></div>
                 <div className='circle circle-4'></div>
             </div>
-       </div>
+        </div>
     )
 }
