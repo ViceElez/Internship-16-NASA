ed642d001e33f4ffab4ae9cb70f5bb5445872c3f
ViceElez <vice.elezovic12@gmail.com>
2025-03-08 22:19:47 +0100

    add loaders

--- nasaPage/src/pages/EarthImageryPage.tsx
+++ nasaPage/src/pages/EarthImageryPage.tsx
@@ -1,130 +1,132 @@
 import { useLocation } from "react-router-dom";
 import "../index.css";
 import { useEffect, useState } from "react";
-import { MapContainer, TileLayer,useMapEvents,Marker  } from "react-leaflet";
+import { MapContainer, TileLayer, useMapEvents, Marker } from "react-leaflet";
 import { Icon, LatLngExpression, DivIcon, point } from "leaflet";
-import 'leaflet/dist/leaflet.css';
-import {getEarthImagery} from "../services/index";
+import "leaflet/dist/leaflet.css";
+import { getEarthImagery } from "../services/index";
 import markerIcon from "../assets/images/marker.png";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import Modal from "../components/Modal";
 
-
-const ClickableMap = ({ onLocationSelect }: { onLocationSelect: (location: { lat: number, lng: number }) => void }) => {
-    useMapEvents({
-        click(e) {
-            const { lat, lng } = e.latlng;
-            onLocationSelect({ lat, lng });
-        },
-    });
-    return null;
+const ClickableMap = ({
+  onLocationSelect,
+  disabled,
+}: {
+  onLocationSelect: (location: { lat: number; lng: number }) => void;
+  disabled: boolean;
+}) => {
+  useMapEvents({
+    click(e) {
+      if (!disabled) {
+        const { lat, lng } = e.latlng;
+        onLocationSelect({ lat, lng });
+      }
+    },
+  });
+  return null;
 };
 
 export const EarthImageryPage = () => {
-    const currentLocation = useLocation();
-    const [selectedCoords, setSelectedCoords] = useState<{ lat: number, lng: number } | null>(null);
-    const [imageUrl, setImageUrl] = useState<string | null>(null);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [favorites, setFavorites] = useState<{ lat: number, lng: number }[]>([]);
+  const currentLocation = useLocation();
+  const [selectedCoords, setSelectedCoords] = useState<{ lat: number; lng: number } | null>(null);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [favorites, setFavorites] = useState<{ lat: number; lng: number }[]>([]);
+  const [loading, setLoading] = useState(false); // LOADING STATE
 
+  const customIcon = new Icon({
+    iconUrl: markerIcon,
+    iconSize: [38, 38],
+  });
 
-    const customIcon=new Icon({
-        iconUrl:markerIcon,
-        iconSize:[38,38]
-    })
+  const createCustomClusterIcon = function (cluster: any) {
+    return new DivIcon({
+      html: `<div class="custom-cluster-icon">${cluster.getChildCount()}</div>`,
+      className: "custom-cluster-icon",
+      iconSize: point(33, 33, true),
+    });
+  };
 
-    const createCustomClusterIcon = function (cluster: any) {
-        return new DivIcon({
-            html: `<div class="custom-cluster-icon">${cluster.getChildCount()}</div>`,
-            className: "custom-cluster-icon",
-            iconSize: point(33, 33,true)
-        });
-    };
+  useEffect(() => {
+    const pageLocationsWithScroll = ["/apod", "/mars-rover", "/neo"];
 
-    useEffect(() => {
-        const pageLocationsWithScroll = [
-            "/apod",
-            "/mars-rover",
-            "/neo"
-        ];
+    if (pageLocationsWithScroll.includes(currentLocation.pathname)) {
+      document.body.style.overflow = "auto";
+    } else {
+      document.body.style.overflow = "hidden";
+    }
+  }, [currentLocation]);
 
-        if (pageLocationsWithScroll.includes(currentLocation.pathname)) {
-            document.body.style.overflow = "auto";
-        } else {
-            document.body.style.overflow = "hidden";
+  useEffect(() => {
+    if (selectedCoords) {
+      const fetchImage = async () => {
+        setLoading(true); // START LOADING
+        try {
+          const fetchedImageUrl = await getEarthImagery(selectedCoords.lat, selectedCoords.lng);
+          if (fetchedImageUrl) {
+            setImageUrl(fetchedImageUrl);
+            setIsModalOpen(true);
+          } else {
+            setImageUrl(null);
+            alert("No image found for the selected coordinates");
+          }
+        } catch (error) {
+          console.error("Error fetching satellite image:", error);
+        } finally {
+          setLoading(false); // STOP LOADING
         }
-    }, [currentLocation]);
+      };
 
-    useEffect(() => {
-        if (selectedCoords) {
-            const fetchImage = async () => {
-                try {
-                    const fetchedImageUrl = await getEarthImagery(selectedCoords.lat, selectedCoords.lng);
-                    if (fetchedImageUrl) {
-                        setImageUrl(fetchedImageUrl);
-                        setIsModalOpen(true);
-                    } else {
-                        setImageUrl(null);
-                        alert("No image found for the selected coordinates");
-                    }
-                } catch (error) {
-                    console.error("Error fetching satellite image:", error);
-                }
-            };
+      fetchImage();
+    }
+  }, [selectedCoords]);
 
-            fetchImage();
-        }
-    }, [selectedCoords]);
+  useEffect(() => {
+    const storedFavorites = localStorage.getItem("favorites")
+      ? JSON.parse(localStorage.getItem("favorites") || "[]")
+      : [];
+    setFavorites(storedFavorites);
+  }, []);
 
-    useEffect(() => {
-        const favorites = localStorage.getItem("favorites")
-          ? JSON.parse(localStorage.getItem("favorites") || "[]")
-          : [];
-        setFavorites(favorites);
-      }, []);
-    
+  const center: LatLngExpression = [51.505, -0.09];
 
-    const center: LatLngExpression = [51.505, -0.09];
+  return (
+    <div className="earth-page-content">
+      {selectedCoords && (
+        <p className="coordinates">
+          Selected Coordinates: {selectedCoords.lat.toFixed(5)}, {selectedCoords.lng.toFixed(5)}
+        </p>
+      )}
 
-    return (
-        <div className="earth-page-content">
-             {selectedCoords && (
-                <p className="coordinates">Selected Coordinates: {selectedCoords.lat.toFixed(5)}, {selectedCoords.lng.toFixed(5)}</p>
-            )}
-            <MapContainer
-                center={center}
-                zoom={13}
-            >
-                <TileLayer  
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {loading && <div className="loader">🚀 Fetching your satellite data... Sit tight! ⏳</div>}
 
-                <ClickableMap onLocationSelect={setSelectedCoords} />
-                <MarkerClusterGroup
-                    key={selectedCoords?.lat}
-                    chunkedLoading
-                    iconCreateFunction={createCustomClusterIcon}
-                >
-                    {favorites.map((marker: { lat: number, lng: number }) => (
-                        <Marker 
-                        key={`${marker.lat}-${marker.lng}`}
-                        position={[marker.lat, marker.lng]} 
-                        icon={customIcon}/>
-                    ))}
-                </MarkerClusterGroup>
-            </MapContainer>
-            <Modal 
-               imageUrl={imageUrl}
-               isOpen={isModalOpen}
-               closeModal={() => setIsModalOpen(false)}
-               lat={selectedCoords?.lat ?? null}
-               lng={selectedCoords?.lng ?? null}
-               setFavorites={setFavorites}/>
-            <div className="circle-container">
-                <div className="circle circle-1"></div>
-                <div className="circle circle-2"></div>
-                <div className="circle circle-3"></div>
-                <div className="circle circle-4"></div>
-            </div>
-        </div>
-    );
+      <MapContainer center={center} zoom={13}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        <ClickableMap onLocationSelect={setSelectedCoords} disabled={loading} />
+        <MarkerClusterGroup key={selectedCoords?.lat} chunkedLoading iconCreateFunction={createCustomClusterIcon}>
+          {favorites.map((marker: { lat: number; lng: number }) => (
+            <Marker key={`${marker.lat}-${marker.lng}`} position={[marker.lat, marker.lng]} icon={customIcon} />
+          ))}
+        </MarkerClusterGroup>
+      </MapContainer>
+
+      <Modal
+        imageUrl={imageUrl}
+        isOpen={isModalOpen}
+        closeModal={() => setIsModalOpen(false)}
+        lat={selectedCoords?.lat ?? null}
+        lng={selectedCoords?.lng ?? null}
+        setFavorites={setFavorites}
+      />
+
+      <div className="circle-container">
+        <div className="circle circle-1"></div>
+        <div className="circle circle-2"></div>
+        <div className="circle circle-3"></div>
+        <div className="circle circle-4"></div>
+      </div>
+    </div>
+  );
 };
--- nasaPage/src/pages/MarsRoverPage.tsx
+++ nasaPage/src/pages/MarsRoverPage.tsx
@@ -1,10 +1,9 @@
 import { useLocation } from 'react-router-dom';
 import '../index.css'
-import { use, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getMarsRoverData } from '../services/index';
 import { MarsRover } from '../components';
-import {handlerFilterChange} from '../types/handleFilterChange';
-
+import { handlerFilterChange } from '../types/handleFilterChange';
 
 interface MarsRoverData {
     id: number;
@@ -19,69 +18,62 @@ interface MarsRoverData {
 
 export const MarsRoverPage = () => {
     const currentLocation = useLocation();
-    const [page,setPage] = useState(1);
+    const [page, setPage] = useState(1);
     const [photos, setPhotos] = useState<MarsRoverData[]>([]);
     const [filteredPhotos, setFilteredPhotos] = useState<MarsRoverData[]>([]);
     const [selectedRover, setSelectedRover] = useState<string>('Default');
     const [selectedCamera, setSelectedCamera] = useState<string>('Default');
+    const [isLoading, setIsLoading] = useState<boolean>(true); // Added loading state
 
     useEffect(() => {
-        const pageLocationsWithScroll=["/apod", "/mars-rover", "/neo", "/earth-imagery"];
-
-        if (pageLocationsWithScroll.includes(currentLocation.pathname)) {
-            document.body.style.overflow = "auto"; 
-        } else {
-            document.body.style.overflow = "hidden";
-        }
+        const pageLocationsWithScroll = ["/apod", "/mars-rover", "/neo", "/earth-imagery"];
+        document.body.style.overflow = pageLocationsWithScroll.includes(currentLocation.pathname) ? "auto" : "hidden";
     }, [currentLocation]);
 
-        useEffect(() => {
-            const fetchData = async () => {
-                const data = await getMarsRoverData(page);
-                setPhotos(data); 
-                setFilteredPhotos(data);
-            };
-            fetchData();
-        }, [page]);
-
-        useEffect(() => {
-            let filtered = photos;
-        
-            if (selectedRover !== 'Default') {
-                filtered = filtered.filter((photo) => photo.rover.name === selectedRover);
-            }
-            
-            if (selectedCamera !== 'Default') {
-                filtered = filtered.filter((photo) => photo.camera.name === selectedCamera);
-            }
-            
-            setFilteredPhotos(filtered);
-        }, [selectedRover, selectedCamera,photos]);
-
-        const handleFilterSelection = (e: React.ChangeEvent<HTMLSelectElement>) => {
-            const selectedFilter = e.target.value;
-    
-            if (selectedFilter === 'Default') {
-                setSelectedRover('Default');
-                setSelectedCamera('Default');
-            } else if (selectedFilter === 'Camera') {
-                setSelectedRover('Default'); 
-            } else if (selectedFilter === 'Rover') {
-                setSelectedCamera('Default');
-            }
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            const data = await getMarsRoverData(page);
+            setPhotos(data);
+            setFilteredPhotos(data);
+            setIsLoading(false); 
         };
+        fetchData();
+    }, [page]);
 
-        useEffect(() => {
-            handlerFilterChange(); 
-        }, []);
+    useEffect(() => {
+        let filtered = photos;
+        if (selectedRover !== 'Default') {
+            filtered = filtered.filter(photo => photo.rover.name === selectedRover);
+        }
+        if (selectedCamera !== 'Default') {
+            filtered = filtered.filter(photo => photo.camera.name === selectedCamera);
+        }
+        setFilteredPhotos(filtered);
+    }, [selectedRover, selectedCamera, photos]);
 
+    const handleFilterSelection = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedFilter = e.target.value;
+        if (selectedFilter === 'Default') {
+            setSelectedRover('Default');
+            setSelectedCamera('Default');
+        } else if (selectedFilter === 'Camera') {
+            setSelectedRover('Default');
+        } else if (selectedFilter === 'Rover') {
+            setSelectedCamera('Default');
+        }
+    };
 
-    return(
+    useEffect(() => {
+        handlerFilterChange();
+    }, []);
+
+    return (
         <div className='mars-page'>
-             <div className="circle-container">
-                 <div className='circle circle-1'></div>
-                 <div className='circle circle-2'></div>
-                 <div className='circle circle-3'></div>
+            <div className="circle-container">
+                <div className='circle circle-1'></div>
+                <div className='circle circle-2'></div>
+                <div className='circle circle-3'></div>
                 <div className='circle circle-4'></div>
             </div>
             <nav>
@@ -91,18 +83,12 @@ export const MarsRoverPage = () => {
                     <option value="Camera">Camera</option>
                     <option value="Rover">Rover</option>
                 </select>
-                <select
-                    id="rover-dropdown" 
-                    className='hidden' 
-                    onChange={(e) => setSelectedRover(e.target.value)}>
-                   <option value="Curiosity">Curiosity</option>
-                   <option value="Opportunity">Opportunity</option>
-                   <option value="Spirit">Spirit</option>
+                <select id="rover-dropdown" className='hidden' onChange={(e) => setSelectedRover(e.target.value)}>
+                    <option value="Curiosity">Curiosity</option>
+                    <option value="Opportunity">Opportunity</option>
+                    <option value="Spirit">Spirit</option>
                 </select>
-                <select 
-                    id="camera-dropdown" 
-                    className='hidden' 
-                    onChange={(e) => setSelectedCamera(e.target.value)}>
+                <select id="camera-dropdown" className='hidden' onChange={(e) => setSelectedCamera(e.target.value)}>
                     <option value="FHAZ">FHAZ</option>
                     <option value="RHAZ">RHAZ</option>
                     <option value="MAST">MAST</option>
@@ -113,25 +99,20 @@ export const MarsRoverPage = () => {
                     <option value="PANCAM">PANCAM</option>
                     <option value="MINITES">MINITES</option>
                 </select>
-
             </nav>
             <div className="mars-page-content">
                 <div className='mars-rover-list'>
-                    {filteredPhotos.length === 0 && <h1 className='no-photos'>No Photos Found</h1>}
-                    {filteredPhotos.map((data)=>(
-                        <MarsRover
-                            key={data.id}
-                            id={data.id}
-                            image={data.img_src}
-                        />
+                    {isLoading && <h1 className='loader'>Loading...</h1>} {}
+                    {!isLoading && filteredPhotos.length === 0 && <h1 className='no-photos'>No Photos Found</h1>}
+                    {!isLoading && filteredPhotos.map((data) => (
+                        <MarsRover key={data.id} id={data.id} image={data.img_src} />
                     ))}
                 </div>
             </div>
             <footer>
-                    <button onClick={() => setPage(page - 1)} disabled={page === 1}>Previous</button>
-                    <button onClick={() => setPage(page + 1)}>Next</button>
+                <button onClick={() => setPage(page - 1)} disabled={page === 1}>Previous</button>
+                <button onClick={() => setPage(page + 1)}>Next</button>
             </footer>
-           
         </div>
-    )
-}
+    );
+};
